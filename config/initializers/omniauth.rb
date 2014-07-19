baba0e17ccec3c0a08d319e2afa4290bcd08266c
panicbus <elementalair@gmail.com>
2014-07-19 10:04:51 -0700

    created view pages

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'] #, {:scope => "publish_actions, publish_stream, read_stream"}
-
+  
 end
